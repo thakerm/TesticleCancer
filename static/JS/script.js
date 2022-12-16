861b52000d1fd60bbed78582b5f2f9593a7720a4
thakerm <thakerm@gmail.com>
2022-12-15 21:37:47 -0800

    initial commit

---
+++ static/JS/script.js
@@ -0,0 +1,11 @@
+function updateForm(path)
+{
+    var selectedText = path.options[path.selectedIndex].innerHTML;
+    var selectedValue = path.value;
+    document.getElementById("stage").disabled=false;
+    
+    if (path.selectedIndex==1)
+    {
+        
+    }
+}
